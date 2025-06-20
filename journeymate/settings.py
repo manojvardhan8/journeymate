7760c5b86f0447b292f237a88167d4590fe36baf
manojvardhan08 <manojvardhansabbavarapu@gmail.com>
2025-06-21 02:28:20 +0530

    d

--- journeymate/settings.py
+++ journeymate/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- ADD THIS LINE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,7 +143,7 @@ STATICFILES_DIRS = [
 # Required if you plan to use collectstatic later (e.g., in production)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
